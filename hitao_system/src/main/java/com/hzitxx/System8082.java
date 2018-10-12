3baaf1e00f9bfc44fb37b185fccc02ba41a4a60c
chenjiawei1996 <1103928383@qq.com>
2018-10-12 14:54:30 +0800

    cjw的adv表

--- hitao_system/src/main/java/com/hzitxx/System8082.java
+++ hitao_system/src/main/java/com/hzitxx/System8082.java
@@ -1,9 +1,10 @@
 package com.hzitxx;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+@MapperScan("com.hzitxx.hitao.mapper")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class System8082 {
---
+++ hitao_system/src/main/java/com/hzitxx/hitao/mapper/ShopAdvMapper.java
@@ -0,0 +1,32 @@
+package com.hzitxx.hitao.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hzitxx.hitao.entity.ShopAdv;
+
+public interface ShopAdvMapper {
+	/**
+	 * 查询广告
+	 * @return
+	 */
+	public List<ShopAdv> findAllShopAdv(Map<String,Object> map);
+	/**
+	 * 添加一条广告
+	 * @param shopAdv
+	 * @return
+	 */
+	public int addShopAdv(ShopAdv shopAdv);
+	/**
+	 * 修改广告数据，包括修改内容和启用和禁用
+	 * @param shopAdv
+	 * @return
+	 */
+	public int updateShopAdv(ShopAdv shopAdv);
+	/**
+	 * 物理删除广告信息
+	 * @param advId
+	 * @return
+	 */
+	public int deleteShopAdv(Integer advId);
+}
