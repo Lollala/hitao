efb72bd883629c25d6936d9e6800c2e41575d84f
chenjiawei1996 <1103928383@qq.com>
2018-10-17 10:29:07 +0800

    11

--- hitao_authc/src/main/java/com/hzitxx/hitao/controller/LoginController.java
+++ hitao_authc/src/main/java/com/hzitxx/hitao/controller/LoginController.java
@@ -1,15 +1,17 @@
 package com.hzitxx.hitao.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
 import com.hzitxx.hitao.entity.ShopAdmin;
 import com.hzitxx.hitao.service.ShopAdminService;
 import com.hzitxx.hitao.utils.JwtTokenUtil;
@@ -29,7 +31,9 @@ public class LoginController {
 			return ServerResponse.createByErrorMessage("登陆失败！用户或密码错误！");
 		} else {
 			String token = JwtTokenUtil.createToken(adminName,shopAdminList.get(0).getAdminId());
-			return ServerResponse.createBySuccess("验证成功！返回token", token);
+			Map<String,String> tokenMap=new HashMap<>();
+			tokenMap.put("token", token);
+			return ServerResponse.createBySuccess("验证成功！返回token",JSON.toJSON(tokenMap));
 		}
 	}
 }
